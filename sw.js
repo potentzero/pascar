14cd59fb04774fdb0bc6762297fcc80a6be71576
Nirmal Kumar R <potentzero@gmail.com>
2026-04-22 05:27:37 +0530

    feat: Add service worker for offline install

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pascar';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Install: Cache the files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch: Serve from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
